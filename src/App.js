a46ecc7c3e6adc9616fdac05a3287c69e9469fef
KimsandraO <kimsandra08@gmail.com>
2023-02-24 11:16:18 +0100

    essage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Footer from "./Components/Footer.";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Footer />
     </div>
   );
 }
