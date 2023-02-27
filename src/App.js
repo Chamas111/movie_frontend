8e40f814aa11613dc2cd7a3d0cbefb4f0d4eab39
unknown <aysamuel007@gmail.com>
2023-02-27 17:36:19 +0100

    moviecards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Movies_Details from "./Components/Movies_Details";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+import Movies_Details from './Components/Movies_Details';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Nav />
+      <Home />
       <Movies_Details />
     </div>
   );
